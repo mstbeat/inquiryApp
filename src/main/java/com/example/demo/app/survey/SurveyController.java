4a1c0c3ecff0207ed14bc33ead545c0b93b23151
mstbeat <mrbrightside82@ymail.com>
2020-06-29 18:15:59 +0900

    finish ch3

--- src/main/java/com/example/demo/app/survey/SurveyController.java
+++ src/main/java/com/example/demo/app/survey/SurveyController.java
@@ -1,5 +1,9 @@
 package com.example.demo.app.survey;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -9,10 +13,30 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import com.example.demo.entity.Survey;
+import com.example.demo.service.SurveyService;
+
 @Controller
 @RequestMapping("/survey")
 public class SurveyController {
-	
+
+	private final SurveyService surveyService;
+
+	@Autowired
+	public SurveyController(SurveyService surveyService) {
+		this.surveyService = surveyService;
+	}
+
+	@GetMapping
+	public String index(Model model) {
+		List<Survey> list = surveyService.getAll();
+
+		model.addAttribute("surveyList", list);
+		model.addAttribute("title", "Survey Index");
+
+		return "survey/index";
+	}
+
 	@GetMapping("/form")
 	public String form(SurveyForm surveyForm,
 			Model model) {
@@ -26,28 +50,36 @@ public class SurveyController {
 		model.addAttribute("title", "Survey Form");
 		return "survey/form";
 	}
-	
+
 	@PostMapping("/confirm")
 	public String confirm(@Validated SurveyForm surveyForm,
 			BindingResult result,
 			Model model) {
-		if(result.hasErrors()) {
+		if (result.hasErrors()) {
 			model.addAttribute("title", "Survey Form");
 			return "survey/form";
 		}
 		model.addAttribute("title", "Confirm Page");
 		return "survey/confirm";
 	}
-	
+
 	@PostMapping("/complete")
 	public String complete(@Validated SurveyForm surveyForm,
 			BindingResult result,
 			RedirectAttributes redirectAttributes,
 			Model model) {
-		if(result.hasErrors()) {
+		if (result.hasErrors()) {
 			model.addAttribute("title", "Survey Form");
 			return "survey/form";
 		}
+		
+		Survey survey = new Survey();
+		survey.setAge(surveyForm.getAge());
+		survey.setSatisfaction(surveyForm.getSatisfaction());
+		survey.setComment(surveyForm.getComment());
+		survey.setCreated(LocalDateTime.now());
+		
+		surveyService.save(survey);
 		redirectAttributes.addFlashAttribute("complete", "Registered!");
 		return "redirect:/survey/form";
 	}
